c287bc1c59ffef4f0d0141ef4d9a664342608c18
Gabrielle Freitas <gabriel.le-freitas@hotmail.com>
2024-12-02 19:24:30 -0300

    :construction: Adicionando texto ao componente inicial

--- src/components/Inicio.vue
+++ src/components/Inicio.vue
@@ -1,8 +1,20 @@
 <script setup>
+import logo from "../assets/logo.svg"
+import loja from "../assets/loja.svg"
 
 </script>
 
 <template>
+    <section>
+        <div>
+            <img :src="logo" alt="">
+            <h1>Uma plataforma de doação para espalhar alegria</h1>
+            <p>Nessas festas de fim de ano mande um presente para a pessoa amada e compartilhe a alegria do Natal.</p>
+            <button>Fazer uma doação</button>
+        </div>
+
+        <img :src="loja" alt="">
+    </section>
 
 </template>
 
